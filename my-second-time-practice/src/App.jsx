49961379cc325f575cd61faebb890413ae92da26
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-08 23:29:51 +0600

    some more react practice

--- my-second-time-practice/src/App.jsx
+++ my-second-time-practice/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Counter from "./Counter";
+import Friends from "./Friends";
+import Users from "./Users";
 
 function App() {
-
   return (
     <>
       <h3>My Second Time Practice</h3>
+
+      <Friends></Friends>
+
+      <Users></Users>
 
       <Counter></Counter>
     </>
