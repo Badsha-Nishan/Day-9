c372ec74a1edaa2f9103ed15e1b39edbbf93b61e
Badsha-Nishan <badshanisan14@gmail.com>
2026-02-09 19:16:37 +0600

    added some style

--- my-second-time-practice/src/MyFriend.jsx
+++ my-second-time-practice/src/MyFriend.jsx
@@ -1,10 +1,11 @@
-export default function ({friend}){
-
-    const {name, email} = friend;
-    return (
-        <div>
-            <h3>Name: {name}</h3>
-            <h3>Email: {email}</h3>
-        </div>
-    )
+export default function ({ friend }) {
+  const { name, email, username, address } = friend;
+  return (
+    <div>
+      <h2 style={{ color: "red", marginTop: "100px" }}>Name: {name}</h2>
+      <h3>Email: {email}</h3>
+      <h4>Username: {username}</h4>
+      <p>City Name: {address.city}</p>
+    </div>
+  );
 }
